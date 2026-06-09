919ed04efa5d60f44b5e6fa2966904bcd089f40e
CossackLucas <lakis.kozak@gmail.com>
2026-06-09 10:41:28 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,324 @@
+'''
+Metadata source plugin using wolnelektury.pl page
+Main definition file
+'''
+import re
+
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
+
+try:
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib import HTTPError
+
+# pylint: disable=import-error
+from calibre.ebooks.metadata.sources.base import Source, Option
+# ToDo: to be removed and replaced with local implementation
+from calibre.ebooks.metadata.sources.base import InternalMetadataCompareKeyGen
+
+from calibre_plugins.wolnelektury_source.main import get_metadata, get_cover_urls, \
+    BaseArgs, access_data, check_site_for_books, WOLNELEKTURY_ID
+# pylint: enable=import-error
+
+PLUGIN_VERSION = (0, 1, 0)
+
+class WolneLekturySource(Source):
+    '''
+    source plugin definition
+    '''
+    name = 'WolneLektury'
+    author = 'Łukasz Kozak'
+    description = 'Pobieranie metadanych i okładek z portalu wolnelektury.pl'
+    version = PLUGIN_VERSION
+    supported_platforms = ['windows', 'osx', 'linux']
+    capabilities = frozenset(['identify', 'cover'])
+    touched_fiedls = frozenset([
+        'title',
+        'authors',
+        'language',
+        'publisher',
+        'cover',
+        'comments',
+        WOLNELEKTURY_ID
+    ])
+
+    has_html_comments = False # ToDo: add as an option
+    supports_gzip_transfer_encoding = False
+    ignore_ssl_errors = False
+    cached_cover_url_is_reliable = True # ToDo: confirm
+    config_help_message = None # ToDo
+    can_get_multiple_covers = True
+    auto_trim_covers = False # ToDo: confirm
+    prefer_results_with_isbn = False
+    options = (
+        # ToDo: can it be limited to max number?
+        Option('max_covers', 'number', 2, ('Maksymalna liczba okładek do pobrania'),
+                      ('Maksymalna liczba okładek do pobrania z portalu')),
+    )
+
+    def is_configured(self):
+        '''
+        probably won't change, defaults will be enough
+        '''
+        return True
+
+    def is_customizable(self):
+        #ToDo
+        return True
+
+    def customization_help(self):
+        raise NotImplementedError
+
+    # ToDo: config_widget & save_settings, maybe validate
+
+    # working methods
+    def get_book_url(self, identifiers):
+        '''
+        Return a 3-tuple or None. The 3-tuple is of the form:
+        (identifier_type, identifier_value, URL).
+        The URL is the URL for the book identified by identifiers at this
+        source. identifier_type, identifier_value specify the identifier
+        corresponding to the URL.
+        This URL must be browsable to by a human using a browser. It is meant
+        to provide a clickable link for the user to easily visit the books page
+        at this source.
+        If no URL is found, return None. This method must be quick, and
+        consistent, so only implement it if it is possible to construct the URL
+        from a known scheme given identifiers.
+
+        There are only to certain identifiers to draw from here:
+        wolnelektury_id and potentialy url itself
+        '''
+        book_id = None
+        book_url = None
+
+        if book_id := identifiers.get(WOLNELEKTURY_ID):
+            book_url = f'https://wolnelektury.pl/katalog/lektura/{book_id}/'
+        elif book_url := identifiers.get('url'):
+            book_id = self.id_from_url(book_url)
+
+        if book_id is not None:
+            return (WOLNELEKTURY_ID, book_id, book_url)
+        return None
+
+    def get_cached_cover_url(self, identifiers):
+        '''
+        Return cached cover URL for the book identified by
+        the identifiers dictionary or None if no such URL exists.
+
+        Note that this method must only return validated URLs, i.e. not URLS
+        that could result in a generic cover image or a not found error.
+        '''
+        # ToDo: should validate the adresses
+        cover_url = None
+        if wolnelektury_id := identifiers.get(WOLNELEKTURY_ID):
+            cover_url = f'https://wolnelektury.pl/media/book/cover/{wolnelektury_id}.jpg'
+        elif result := self.get_book_url(identifiers):
+            cover_url = f'https://wolnelektury.pl/media/book/cover/{result[1]}.jpg'
+        if cover_url:
+            # ToDo: make sure it makes sense
+            with access_data(self.browser.open_novisit(cover_url, timeout=10), None) as page:
+                if page.getcode() != 200:
+                    cover_url = None
+        return cover_url
+
+    WOLNELEKTURY_ID_REGEX = re.compile(
+        r'(https?:\/\/)(www.)?wolnelektury.pl\/katalog\/lektura\/([a-z\-]+)\/?'
+    )
+
+    def id_from_url(self, url):
+        '''
+        Parse a URL and return a tuple of the form:
+        (identifier_type, identifier_value).
+        If the URL does not match the pattern for the metadata source,
+        return None.
+        '''
+        search_result = self.WOLNELEKTURY_ID_REGEX.search(url)
+        if search_result is not None:
+            return search_result.group(3)
+
+        return None
+
+    # pylint: disable=dangerous-default-value
+    def identify_results_keygen(self, title=None, authors=None,
+            identifiers={}):
+        '''
+        Return a function that is used to generate a key that can sort Metadata
+        objects by their relevance given a search query (title, authors,
+        identifiers).
+
+        These keys are used to sort the results of a call to :meth:`identify`.
+
+        For details on the default algorithm see
+        :class:`InternalMetadataCompareKeyGen`. Re-implement this function in
+        your plugin if the default algorithm is not suitable.
+        '''
+        # ToDo: prepare my version
+        def keygen(mi):
+            return InternalMetadataCompareKeyGen(mi, self, title, authors,
+                identifiers)
+        return keygen
+    # pylint: enable=dangerous-default-value
+
+    # pylint: disable=too-many-positional-arguments, too-many-arguments, dangerous-default-value
+    def identify(self, log, result_queue, abort, title=None, authors=None,
+            identifiers={}, timeout=30):
+        '''
+        Identify a book by its Title/Author/ISBN/etc.
+
+        If identifiers(s) are specified and no match is found and this metadata
+        source does not store all related identifiers (for example, all ISBNs
+        of a book), this method should retry with just the title and author
+        (assuming they were specified).
+
+        If this metadata source also provides covers, the URL to the cover
+        should be cached so that a subsequent call to the get covers API with
+        the same ISBN/special identifier does not need to get the cover URL
+        again. Use the caching API for this.
+
+        Every Metadata object put into result_queue by this method must have a
+        `source_relevance` attribute that is an integer indicating the order in
+        which the results were returned by the metadata source for this query.
+        This integer will be used by :meth:`compare_identify_results`. If the
+        order is unimportant, set it to zero for every result.
+
+        Make sure that any cover/ISBN mapping information is cached before the
+        Metadata object is put into result_queue.
+
+        :param log: A log object, use it to output debugging information/errors
+        :param result_queue: A result Queue, results should be put into it.
+                            Each result is a Metadata object
+        :param abort: If abort.is_set() returns True, abort further processing
+                      and return as soon as possible
+        :param title: The title of the book, can be None
+        :param authors: A list of authors of the book, can be None
+        :param identifiers: A dictionary of other identifiers, most commonly
+                            {'isbn':'1234...'}
+        :param timeout: Timeout in seconds, no network request should hang for
+                        longer than timeout.
+        :return: None if no errors occurred, otherwise a unicode representation
+                 of the error suitable for showing to the user
+        '''
+        if abort.is_set():
+            return None
+
+        log.info('Identification of a book')
+
+        wolnelektury_id = identifiers.get(WOLNELEKTURY_ID)
+        if (wolnelektury_id is None) and (search_result := self.get_book_url(identifiers)):
+            wolnelektury_id = search_result[1]
+        if abort.is_set():
+            return None
+
+        base_args = BaseArgs(abort, self.browser, log, timeout)
+        found_books = []
+        if wolnelektury_id is None:
+            log.debug('Preliminary identification failed. Complex search starts')
+            found_books = check_site_for_books(
+                base_args,
+                self.get_title_tokens(title),
+                self.get_author_tokens(authors)
+            )
+        else:
+            log.debug('Preliminary identification was a success')
+            found_books = [wolnelektury_id]
+
+        if abort.is_set():
+            return None
+
+        if len(found_books) == 0:
+            return 'Book could not be identified on wolnelektury.pl'
+
+        no_books = 0
+        for book_id in found_books:
+            if me := get_metadata(base_args, book_id):
+                # ToDo: For some reason I got only one result, even if mroe are found!
+                me.set_identifier(WOLNELEKTURY_ID, book_id)
+                result_queue.put(me)
+                no_books += 1
+                log.info(
+                    f'Metadata for "{book_id}" id found on the site'
+                )
+            else:
+                log.error(
+                    f'Metadata could not be found for "{book_id}" id on the site'
+                )
+            if abort.is_set():
+                return None
+
+        log.info(f'{no_books} books metadata were downloaded')
+
+        return None
+
+    # pylint: enable=too-many-positional-arguments, too-many-arguments, dangerous-default-value
+
+    # pylint: disable=too-many-positional-arguments, too-many-arguments, dangerous-default-value
+    def download_cover(self, log, result_queue, abort,
+            title=None, authors=None, identifiers={}, timeout=30, get_best_cover=False):
+        '''
+        Download a cover and put it into result_queue. The parameters all have
+        the same meaning as for :meth:`identify`. Put (self, cover_data) into
+        result_queue.
+
+        This method should use cached cover URLs for efficiency whenever
+        possible. When cached data is not present, most plugins simply call
+        identify and use its results.
+
+        If the parameter get_best_cover is True and this plugin can get
+        multiple covers, it should only get the "best" one.
+        '''
+        log.debug('Downloading cover')
+        if get_best_cover and (cover_url := self.get_cached_cover_url(identifiers)):
+            result_queue.put((self, cover_url))
+            log.debug('Downloaded best cover')
+            return None
+
+        wolnelektury_id = identifiers.get(WOLNELEKTURY_ID)
+        identify_queue = Queue()
+        found_books: list[str] = []
+
+        if wolnelektury_id is None:
+            result = self.identify(log, identify_queue, abort, title, authors, identifiers, timeout)
+            if abort.is_set():
+                return None
+
+            if result is not None:
+                return result
+            if not identify_queue.empty:
+                found_books = [book_id for me in iter(identify_queue.get_nowait, None) \
+                    if (book_id := me.get_identifiers().get(WOLNELEKTURY_ID))]
+        else:
+            found_books = [wolnelektury_id]
+
+        if len(found_books) == 0:
+            return 'No books were identified to find covers'
+
+        urls: list[str] = []
+        base_args = BaseArgs(abort, self.browser, log, timeout)
+        for book_id in found_books:
+            urls.extend(get_cover_urls(base_args, wolnelektury_id, get_best_cover))
+        log.info(f'{len(urls)} cover urls found')
+
+        if abort.is_set():
+            return None
+        self.download_multiple_covers(
+            title,
+            authors,
+            urls,
+            get_best_cover,
+            timeout,
+            result_queue,
+            abort,
+            log
+        )
+
+        return None
+
+    # pylint: enable=too-many-positional-arguments, too-many-arguments, dangerous-default-value
+
+# ToDo: tests
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,267 @@
+'''
+Custom functions used by plugin
+'''
+# pylint: disable=c-extension-no-member
+import json
+import re
+
+from typing import Optional
+from contextlib import contextmanager
+from collections import namedtuple
+from datetime import datetime
+
+try:
+    from urllib.error import HTTPError, URLError
+except ImportError:
+    from urllib import HTTPError, URLError
+try:
+    from urllib.parse import quote_plus
+except ImportError:
+    from urlib import quote_plus
+
+from socket import gaierror
+
+# lxml.etree does not have exposed c-module
+from lxml import etree
+from lxml.html import fromstring, Element
+
+# pylint: disable=import-error
+from calibre.ebooks.metadata.book.base import Metadata
+# pylint: enable=import-error
+
+WOLNELEKTURY_ID = 'wolnelektury_id'
+MAX_RESULTS = 3
+
+@contextmanager
+def access_data(thing, log=None):
+    '''
+    context manager trying to service all possible issues when getting data via Internet
+    '''
+    try:
+        yield thing
+    #ToDo: URLError and gaierror do not work for browser, crash still happens when there's not connection
+    except (HTTPError, URLError, gaierror) as e:
+        if log is not None:
+            log.exception(f'Network error:\n{e}')
+    except AttributeError as e:
+        if log is not None:
+            log.exception(f'{e}')
+    except json.JSONDecodeError:
+        if log is not None:
+            log.exception(f'Error decoding data:\n{e}')
+    except UnicodeDecodeError:
+        if log is not None:
+            log.exception(f'Error decoding data:\n{e}')
+    except etree.LxmlError as e:
+        if log is not None:
+            log.exception(f'Error parsing using lxml:\n{e}')
+    finally:
+        thing.close()
+
+BaseArgs = namedtuple('BaseArgs', ['abort', 'browser', 'log', 'timeout'])
+
+def get_metadata(base_args: BaseArgs, wolnelektury_id: str) -> Optional[Metadata]:
+    '''
+    gets metadata from wolnelektury for given book by it's id
+    '''
+    abort, browser, log, timeout = base_args
+
+    if abort.is_set():
+        return None
+
+    wolnelektury_url: str = __get_xml_url(wolnelektury_id)
+    me = None
+    with access_data(browser.open(wolnelektury_url, timeout=timeout), log) as page:
+        if abort.is_set():
+            return None
+        log.debug(f'Page \'{wolnelektury_url}\' accessed and parsed')
+        read_data = page.read().decode(encoding='utf-8')
+        me = __extract_metadata_xml(etree.fromstring(read_data))
+
+    return me
+
+# 'cover' has to stay first to break for best cover to work properly
+COVER_NAMES = ('cover', 'simple_cover')
+def get_cover_urls(base_args: BaseArgs, wolnelektury_id: str, get_best_cover=False) -> tuple:
+    '''
+    get cover's urls from wolnelektury.pl. If none are found, result is empty
+    '''
+    abort, browser, log, timeout = base_args
+
+    log.debug(f"Getting cover urls for {wolnelektury_id}")
+    if abort.is_set():
+        return ()
+    result: list[str] = []
+
+    source_url: str = __get_api_url(wolnelektury_id)
+
+    with access_data(browser.open(source_url, timeout=timeout), log) as page:
+        log.debug("Parsing data")
+        parsed_data = json.load(page)
+        for name in COVER_NAMES:
+            if abort.is_set():
+                break
+            url = parsed_data.get(name)
+            if url is not None:
+                result.append(url)
+            if get_best_cover:
+                break
+
+    return tuple(result)
+
+def check_site_for_books(
+    base_args: BaseArgs,
+    title_tokens: list[str],
+    author_tokens: list[str]
+    ) -> list[str]:
+    '''
+    perform search on wolnelektury site, and returns ids
+
+    proposed algorithm: check by title, the those results by author
+    then author and look for a book
+    '''
+    abort, browser, log, timeout = base_args
+
+    if abort.is_set():
+        return []
+    # ToDo: should we used queries using =&category=book ?
+    title_query: str = 'https://wolnelektury.pl/szukaj/?q=' + quote_plus(' '.join(title_tokens))
+    log.info(f'Checking query: {title_query}')
+
+    found_books: list[str] = []
+    with access_data(browser.open(title_query, timeout=timeout), log) as page:
+        if abort.is_set():
+            return []
+        parsed_data = fromstring(page.read().decode(encoding='utf-8'))
+        found_books = __extract_books(parsed_data)
+        log.info(f'{len(found_books)} book(s) were found') 
+        # ToDo: finish check
+        #found_books = __check_found_books(found_books, author_tokens)
+
+    if len(found_books) != 0:
+        return found_books
+
+    return []
+
+    # ToDo: expand search
+    raise NotImplementedError('Looking for an author should be checked')
+    author_query: str = 'https://wolnelektury.pl/szukaj/?q=' + quote_plus(' '.join(author_tokens))
+    with access_data(browser(author_query, timeout=timeout), log) as page:
+        found_authors = __extract_authors(page)
+        if abort.is_set():
+            return []
+
+        found_books = __extract_authors_books(found_authors)
+
+    return found_books
+
+ID_REGEX = re.compile(r'/katalog/lektura/([a-z\-]+)/')
+
+def __extract_books(parsed_data: Element) -> list[str]:
+    result = []
+    xpath:str = './/article[@class=\'l-books__item book-container-activator\']'
+    found = MAX_RESULTS
+    # ToDo: check iter_find instead of findall
+    for element in parsed_data.findall(xpath):
+        if found == 0:
+            break
+        book_url: str = element[0][0].get('href')
+        if book_match := ID_REGEX.match(book_url):
+            result.append(book_match[1])
+            found -= 1
+
+    return result
+
+def __check_found_books(found_books: list[str], author_tokens: list[str]) -> list[str]:
+    result: list[str] = []
+    # ToDo: check, if authors are matching for given book
+    for book in found_books:
+        print(book)
+    raise NotImplementedError(f'__check_found_books not implemented, {author_tokens}')
+    return result
+
+def __extract_authors(page) -> list[str]:
+    result = []
+    raise NotImplementedError(f'__extract_authors not implemented, {page}')
+    # ToDo: search for  <ul class="c-search-result c-search-result-author">
+    return result
+
+def __extract_authors_books(authors_list: list[str]) -> list[str]:
+    result = []
+    # ToDo: check <article class="l-books__item book-container-activator" data-pop="-79" data-longpress="hover">
+    for author in authors_list:
+        print(author)
+    raise NotImplementedError('__extract_authors_book not implemented')
+    return result
+
+def __get_api_url(wolnelektury_id: str) -> str:
+    ''' 
+    Generate api url from wolnelektury id (nothing is checked)
+    '''
+    return f'https://wolnelektury.pl/api/books/{wolnelektury_id}/?format=json'
+
+def __get_xml_url(wolnelektury_id: str) -> str:
+    ''' 
+    Generate url for data xml file from wolnelektury id (nothing is checked)
+    '''
+    return f'https://wolnelektury.pl/media/book/xml/{wolnelektury_id}.xml'
+
+def __extract_metadata_xml(parsed_data: etree.Element) -> Metadata:
+    me = Metadata('', '')
+
+    if book_title := __get_data_from_xml(parsed_data, 'title'):
+        me.title = book_title
+
+    book_authors = __get_authors_from_parsed_xml(parsed_data)
+    me.authors = book_authors if len(book_authors) != 0 else [('Unknown')]
+
+    if book_lang := __get_data_from_xml(parsed_data, 'language'):
+        me.language = book_lang
+
+    if book_date := __get_date_from_parsed_xml(parsed_data,'date'):
+        me.pubdate = book_date
+
+    if book_abstract := __get_abstract(parsed_data):
+        me.comments = book_abstract
+
+    me.publisher = 'Fundacja Nowoczesna Polska'
+
+    return me
+
+def __get_data_from_xml(parsed_data: etree.Element, element: str) -> Optional[str]:
+    found_data = parsed_data.find('.//{*}' + element)
+    return None if found_data is None else found_data.text
+
+def __get_authors_from_parsed_xml(parsed_data: etree.Element) -> list[str]:
+    found_data = parsed_data.findall('.//{*}creator')
+    if len(found_data) == 0:
+        return []
+
+    result: list[str] = []
+    for reversed_author in found_data:
+        result.append(__standardize_author(reversed_author.text))
+
+    return result
+
+def __get_date_from_parsed_xml(parsed_data: etree.Element, elemet: str) -> Optional[datetime]:
+    found_date = ''
+    if not (found_date := __get_data_from_xml(parsed_data, elemet)):
+        return None
+    date_list = found_date.split('-')
+    return datetime(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+
+def __standardize_author(reversed_name: str) -> str:
+    if ',' not in reversed_name:
+        return reversed_name
+    elements = reversed_name.split(',')
+    return f"{elements[1].strip()} {elements[0]}"
+
+def __get_abstract(parsed_data: etree.Element) -> Optional[str]:
+    result: str = ''
+    for paragraph in parsed_data.findall('.//akap'):
+        # ToDo: use it to get test with formatting
+        #pseudo_html = tostring(paragraph, encoding="utf-8").decode(encoding="utf-8")
+        result += ''.join(paragraph.itertext())
+        result += '\n\n'
+
+    return None if len(result) == 0 else result
